525db58bd4d17dc3a625fcba44a6b728f6372edc
Janakshan <janaksan.jana@gmail.com>
2024-11-22 14:11:04 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to MyFi</Text>
+      <Text style={styles.description}>
+        Best Personal Finance and Budgeting Apps
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+  },
+  description: {
+    fontSize: 14,
+  },
+});
